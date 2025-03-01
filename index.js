990f303625f1b7333ad9dca71ab6d60c5a107adc
hamdytoto <hamdytoto>
2025-03-02 00:06:33 +0200

    adding the socket io part

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ import express from "express";
 import bootstrap from "./src/app.controller.js";
 import chalk from "chalk";
 import dotenv from "dotenv";
+import { Server } from "socket.io";
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 bootstrap(app, express);
-app.listen(port, () => {
-	console.log(chalk.red.bgGreen.underline.bold(`Example app listening on port ${port}!`));
+const server = app.listen(port, () => {
+	console.log(
+		chalk.red.bgGreen.underline.bold(`Example app listening on port ${port}!`)
+	);
+});
+
+// Socket Connection 
+const io = new Server(server, {
+	cors: {
+		origin: "*",
+	},
 });
